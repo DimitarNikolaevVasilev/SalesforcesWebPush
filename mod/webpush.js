27960ee97d0ded5d465f5c57a0bc84661c7a4ded
DimitarNikolaevVasilev <49063656+DimitarNikolaevVasilev@users.noreply.github.com>
2019-05-23 12:41:50 +0200

    Update webpush.js

--- mod/webpush.js
+++ mod/webpush.js
@@ -1,7 +1,7 @@
 var webpush = require('web-push');
 //const vapidKeys = webpush.generateVAPIDKeys();
 
-webpush.setGCMAPIKey(process.env.gcmkey);
+//webpush.setGCMAPIKey(process.env.gcmkey);
 webpush.setVapidDetails(
 	'mailto:' + process.env.email,
 	process.env.vapid_public,
